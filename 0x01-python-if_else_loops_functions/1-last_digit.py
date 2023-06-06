6df87e88801d4f8653bb2de7e356e32593c3096f
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-06 05:08:30 -0700

    removed line calling str() to covert type(number)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-string_digit = str(number)
 last_digit = number % 10 if number > 0 else number % -10
 if int(last_digit) > 5:
     print(f'Last digit of {number} is {last_digit} and is greater than 5')
