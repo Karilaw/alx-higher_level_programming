c93be9734f2a93cb508fc579bcd97475c0b4b173
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-08 05:00:55 -0700

    third commit to test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
         print("0 arguments.")
-    if num_args == 1:
-        print("1 argument:".format(num_args))
+    elif num_args == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(num_args))
     for i, arg in enumerate(sys.argv[1:]):
