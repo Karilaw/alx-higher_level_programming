fced3a1c8b7bda1623332e2a665e4becc080ed0b
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-12 03:50:41 -0700

    first commit to test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length < 0:
+    if length == 0:
         first = sentence[0]
-        return (length, first)
+        return (0, None)
     else:
         first = sentence[0]
         return (length, first)
