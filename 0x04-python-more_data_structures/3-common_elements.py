c2c1766b8812f8004f65a8e202253c8dcc9d03e9
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-14 05:52:38 -0700

    second commit to test

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i in set_2 and j in set_1:
-                return i
+    return set_1 & set_2
