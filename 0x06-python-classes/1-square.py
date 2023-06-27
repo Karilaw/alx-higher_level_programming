e83fd41b158a88b5c9f7c6b5145850cbbbbf9ba1
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-27 06:03:33 -0700

    third commit to test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,8 @@
 class Square:
     """Represent a square"""
     def __init__(self, size):
-        """Initialize the square with the given size.
+        """Initialize a new Square.
+
         Args
         size(int) The size of the new square
         """
