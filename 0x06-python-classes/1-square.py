63c0eaa3aa3528ca5fe59d47acdca02c3c0dd629
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-27 05:48:59 -0700

    first commit to test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining a class square"""
+
+
+class Square:
+    """This class defines a square with a private instance attribute size."""
+    def __init__(self, size):
+        """Initialize the square with the given size."""
+        self._size = size
