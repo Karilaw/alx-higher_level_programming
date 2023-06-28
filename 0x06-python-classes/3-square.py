f1f1f4a1211b31eefd9040a83f51a0c9ab4b335d
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-28 05:43:08 -0700

    Updated code to fit pycode format

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,14 +4,14 @@
 
 class Square:
     """This class defines a square with a private instance attribute size."""
-    def __init__(self, size=0):
 
+    def __init__(self, size=0):
         """Initialize the square with the given size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size=size
+        self.__size = size
 
     def area(self):
         """Return the current square area."""
