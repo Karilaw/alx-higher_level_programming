f3fd183e4e870abb88e4c75cb81ef2b1ed47dd6b
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-28 05:49:13 -0700

    updated code to correct pyccode format

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square"""
+
+
 class Square:
     """This class defines a square with a private instance attribute size."""
     def __init__(self, size=0):
@@ -19,6 +21,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Return the current square area."""
         return self.__size ** 2
