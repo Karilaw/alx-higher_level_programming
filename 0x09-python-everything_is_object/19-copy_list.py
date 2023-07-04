46831384885139191a58d930e2c851d83efadb25
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-04 05:03:27 -0700

    Commiting to check answers

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module to return a copy of list"""
+
+
+def copy_list(l):
+    """
+    A fuction that retuns copy of list
+
+    Args:
+        l(list): a list
+    Return:
+        A copy of list l
+    """
+    return l[:]
