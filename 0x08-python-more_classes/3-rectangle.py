931bdada44cc33e3b603d8a620f77e2e018bfa90
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-05 02:13:57 -0700

    task pycode style correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,14 +58,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """Get the area of Rectangle"""
         return self.__height * self.__width
+
     def perimeter(self):
         """Get the perimeter of Rectangle"""
         if self.width == 0 or self.height == 0:
             return 0
         return (self.__width + self.__height) * 2
+
     def __str__(self):
         """Return a string representation of the rectangle"""
         if self.width == 0 or self.height == 0:
