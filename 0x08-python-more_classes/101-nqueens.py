d397252850849c7d35f3159bde52ae3042e74ad4
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-05 03:39:20 -0700

    first commit to test

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import sys
+
+def can_place(board, row, col):
+    """
+    Check if a queen can be placed in a given position.
+
+    Args:
+        board (list): A list representing
+        row (int): The row index of the position to check.
+        col (int): The column index of the position to check.
+
+    Returns:
+        bool: True if a queen can be placed, False otherwise.
+    """
+    for i in range(row):
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
+            return False
+    return True
+
+def solve(board, row):
+    """
+    Find all solutions to the N queens problem using backtracking.
+
+    Args:
+        board (list): A list representing the current state of the chessboard.
+        row (int): The current row to place a queen in.
+
+    Returns:
+        None
+    """
+    n = len(board)
+    if row == n:
+        print([[i, board[i]] for i in range(n)])
+        return
+
+    for col in range(n):
+        if can_place(board, row, col):
+            board[row] = col
+            solve(board, row + 1)
+            board[row] = -1
+
+def main():
+    """
+    Parse command line arguments and solve the N queens problem.
+
+    Returns:
+        None
+    """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve(board, 0)
+
+if __name__ == "__main__":
+    main()
