b853b82d6e1535b3cadd56a7d1726e26d9f5dee6
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-05 04:51:19 -0700

    task 5 correction

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""This is unit test module"""
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
@@ -5,21 +9,15 @@ class TestMaxInteger(unittest.TestCase):
     """Test cases for the max_integer function"""
 
     def test_max_integer(self):
-        """Test max_integer with lists of integers"""
+        """Test normal input"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([1, 1, 1, 1]), 1)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([4]), 4)
 
     def test_empty_list(self):
-        """Test max_integer with an empty list"""
-        self.assertIsNone(max_integer([]))
-
-    def test_non_list(self):
-        """Test max_integer with a non-list argument"""
-        with self.assertRaises(TypeError):
-            max_integer("not a list")
+        """Test empty list input"""
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
