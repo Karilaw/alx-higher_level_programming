04f2fc61e0ec4dfdc8aa3006e6bfad1c9b2a8899
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-10 04:03:51 -0700

    third commit to test

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""A module for list of available attributes and methods of an object"""
+"""A module for list of available
+attributes and methods of an object"""
 
 
 def lookup(obj):
-    """A function that returns the list of available attributes and methods of an object"""
+    """A function that returns the list of available
+    attributes and methods of an object"""
     return dir(obj)
