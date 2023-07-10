e2c165ad6d1424ccdb8bbda433903ef82f124fb8
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-10 06:02:56 -0700

    first commit to test

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module defining class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class which implement method area"""
+
+    def area(self):
+        """a method to get area"""
+        raise Exception("area() is not implemented")
