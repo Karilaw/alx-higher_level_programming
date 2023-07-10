204981c3f2d8ea1374e646937b5ed87f2963d5c3
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-10 05:14:54 -0700

    second commit to test

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Defining a module that check if obj 
+"""Defining a module that check if obj
 is instance of class
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """returns True if the object is an instance of,
-    or if the object is an instance of a class that
-    inherited from, the specified class
-    otherwise False
+    """returns True if the object is
+    an instance of class
     """
     return isinstance(obj, a_class)
