6bff5cc786050436ba3cc33172e05a733b6a6636
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-10 14:23:01 -0700

    first commit to test

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A module that defines class square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new Square with the given size.
+
+        :param size: The size of the square.
+        :type size: int
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates and returns the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns string rep of object"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
