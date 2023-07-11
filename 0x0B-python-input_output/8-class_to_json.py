d6f4dcde859523cd864c7b64128cd0aef9ad7aff
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-11 07:00:20 -0700

    first commit to test

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module that defines a function
+that describes json data
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure for JSON serialization of an object
+
+    :param obj: an instance of a Class
+    :return: the dictionary description of the object
+    """
+    return obj.__dict__
