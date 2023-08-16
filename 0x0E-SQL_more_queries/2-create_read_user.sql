bd8d7348158edf8b0bc595dae5a191638c556df8
Karilaw <lawrencekariithi6@gmail.com>
2023-08-16 02:58:48 -0700

    commiting task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Create database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create user user_0d_2 if it does not exist, and set its password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant SELECT privilege to user user_0d_2 on database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
