362dfa881793a7fa3f96d5b6bc6f405352a17fdb
Karilaw <lawrencekariithi6@gmail.com>
2023-09-12 11:42:12 +0000

    commit task to test

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Handle the case where w or h are not positive integers
+    }
+  }
+}
+
+module.exports = Rectangle;
