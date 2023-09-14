07c095e224be4c3c2cf570d57f3c6dd835be9e8b
Karilaw <lawrencekariithi6@gmail.com>
2023-09-14 09:12:36 +0000

    task commit to test

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""lists  all states from the database hbtn_oe_o_usa"""
+
+if __name__ == "__main__":
+	import MYSQLdb
+	import sys
+
+	db = MYSQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	cur = db.cursor()
+	cur.execute("SELECT * FROM states ORDER BY id ASC")
+	rows = cur.fetchall()
+	for row in rows:
+		print(row)
+	cur.close()
+	db.close()
