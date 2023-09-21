81f753348c8129310053e0c0818a8987fc6719b5
Karilaw <lawrencekariithi6@gmail.com>
2023-09-21 02:22:27 -0700

    commit correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-script that lists all states from db where
-name matches the argument
+Script that lists all states from the database hbtn_0e_0_usa where
+the name matches the argument.
 """
 
+# Import necessary modules
 import MySQLdb
 import sys
 
+# Main function
 if __name__ == "__main__":
+    # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,14 +18,27 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
+    # Create a cursor object
     cur = db.cursor()
-    cur.execute(
+
+    # Define the query
+    query = (
         "SELECT * FROM states "
-        "WHERE name = %s "
-        "ORDER BY states.id ASC", (sys.argv[4],)
-    )
+        "WHERE name = '{}' "
+        "ORDER BY states.id ASC"
+    ).format(sys.argv[4])
+
+    # Execute the query
+    cur.execute(query)
+
+    # Fetch all rows
     rows = cur.fetchall()
+
+    # Print each row
     for row in rows:
         print(row)
+
+    # Close cursor and database connection
     cur.close()
     db.close()
