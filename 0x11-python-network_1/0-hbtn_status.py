8d44ed53031379ccaebcc476780caad70600c90a
Karilaw <lawrencekariithi6@gmail.com>
2023-09-29 00:00:15 -0700

    first commit to test

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" A module to fetch request """
+import urllib.request
+
+
+def fetch_url(url):
+    """
+    fetches a given URL using the urllib package in Python
+
+    Args:
+        url (str): the url of resource to fetch
+
+    Prints:
+        str: the body of response, showing its type, content
+        and utf8 content.
+    """
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+        print("Body response")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+
+url = "https://alx-intranet.hbtn.io/status"
+fetch_url(url)
